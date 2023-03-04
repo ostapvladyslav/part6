3048252e642f3bf2474ea866be2f3cf07daeb81b
Vladyslav Ostapchuk <ostapvladyslav@gmail.com>
2023-03-05 00:36:51 +0200

    Exercise 6.5

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -25,15 +25,17 @@ const App = () => {
         <button type='submit'>add</button>
       </form>
 
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+      {anecdotes
+        .sort((a, b) => b.votes - a.votes)
+        .map((anecdote) => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       <h2>create new</h2>
       <form>
         <div>
